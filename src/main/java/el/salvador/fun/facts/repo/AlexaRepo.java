8e46ceb0e51fa29ef6f93337694f495c356fed37
yherrera001 <yherrera001@my.wilmu.edu>
2020-11-29 23:54:45 -0500

    Updated

--- src/main/java/el/salvador/fun/facts/repo/AlexaRepo.java
+++ src/main/java/el/salvador/fun/facts/repo/AlexaRepo.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Repository;
 public interface AlexaRepo extends CrudRepository<FunFacts, String> {
 
     //We can use also native SQL to define our query.
-    // All we have to do is set the value of the nativeQuery attribute to true and define the native SQL query in the value attribute of the annotation:
-
-    @Query(value = "SELECT TOP 1 Fact FROM FUNFACTS WHERE FactType = ?1 ORDER BY RAND()", nativeQuery = true) //Taking
+    //All we have to do is set the value of the nativeQuery attribute to true and define the native SQL query in the value attribute of the annotation:
+    //You can also add a parameter of type Sort to your method definition. Spring Data JPA will then generate the required ORDER BY clause
+    @Query(value = "SELECT TOP 1 Fact FROM FUNFACTS WHERE FactType = ?1 ORDER BY RAND()", nativeQuery = true)
     public String getAllFacts(String intentType);
 
 
